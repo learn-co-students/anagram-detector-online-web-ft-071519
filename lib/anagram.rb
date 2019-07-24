85aa2b7a802a2cdae729d81274865e0a0beced70
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-24 19:52:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(sentence)
+   resultArray =[]
+    wordArray = @word.split("").sort
+    #sentenceArray = sentence.split(" ")
+    sentence.each do|sentenceWord| 
+#     binding.pry
+      if sentenceWord.split("").sort == wordArray
+        resultArray << sentenceWord
+      end
+    end
+    return resultArray
+  end
+end
